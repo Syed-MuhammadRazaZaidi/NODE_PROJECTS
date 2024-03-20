f8488a8c27e38032dfb36707c11a9913aa5fc907
Syed-MuhammadRazaZaidi <160251042+Syed-MuhammadRazaZaidi@users.noreply.github.com>
2024-03-21 02:57:38 +0500

    Update calc.js

--- project00_simple-calculator/calc.js
+++ project00_simple-calculator/calc.js
@@ -1,42 +1,3 @@
-// import inquirer from "inquirer";
-// const inputs:{
-//     first_number : number,
-//     second_number : number,
-//     operator: string,
-// } = await inquirer.prompt([
-//     {
-//         type: "number",
-//         name: "first_number",
-//         message: "Enter your first number",
-//     },
-//     {
-//         type: "list",
-//         name: "operator",
-//         choices: ["+","-","*","/"],
-//         message: "Select Operator ",
-//     },
-//     {
-//         type: "number",
-//         name: "second_number",
-//         message: "Enter your second number",
-//     }
-// ])
-// console.log(inputs);
-// if (inputs.first_number && inputs.operator && inputs.second_number ) {
-//         let result:number = 0 
-//     if (inputs.operator === "+") {
-//         result = inputs.first_number + inputs.second_number;
-//     }else  if (inputs.operator === "-") {
-//         result = inputs.first_number - inputs.second_number;
-//     }else  if (inputs.operator === "*") {
-//         result = inputs.first_number * inputs.second_number;
-//     }else  if (inputs.operator === "/") {
-//         result = inputs.first_number / inputs.second_number;
-//     }
-//     console.log("Your result is: ", result);
-// }else {
-//     console.log("Kindly enter valid input");
-// }
 import chalk from "chalk";
 import inquirer from "inquirer";
 const userInput = await inquirer.prompt([
@@ -78,5 +39,4 @@ if (userInput.first_value && userInput.operator && userInput.second_value) {
 }
 else {
     console.log(chalk.red("Kindly Provide The Required Inputs"));
-}
-;
+};
