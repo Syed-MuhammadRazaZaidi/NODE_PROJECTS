41521ada9f37de49522c3b2b110aaa64777e06fe
Syed-MuhammadRazaZaidi <160251042+Syed-MuhammadRazaZaidi@users.noreply.github.com>
2024-03-21 16:38:12 +0500

    Add files via upload
    This guess the number game is a short TypeScript/Node.js project that allows the user to guess the number generated by the computer. There are also several ways to alter the game, like adding more rounds or displaying the score. It’s quite simple and uses the random function to generate a number.

---
+++ project01_number_guessing-game/index.js
@@ -0,0 +1,35 @@
+#! /usr/bin/env node
+// NUMBER GUESSING GAME:-
+import inquirer from "inquirer";
+import chalk from "chalk";
+let score = 0;
+let play = true;
+async function guessingGame() {
+    while (play) {
+        let compGeneratedNum = Math.round((Math.random() * 2 + 1));
+        let userNum = await inquirer.prompt({
+            type: "number",
+            name: "userSelection",
+            message: "Guess computer generated number from 1 to 3:"
+        });
+        if (userNum.userSelection) {
+            if (userNum.userSelection === compGeneratedNum) {
+                console.log(chalk.green("Congrats! You guessed the right number"));
+                console.log("You get 10 score points");
+                score += 10;
+            }
+            else {
+                console.log(chalk.red("Please try again!"));
+                play = false;
+            }
+            console.log(chalk.blue(`Computer Generated Number is:   ${compGeneratedNum}`));
+            console.log(chalk.yellow(`Total Score Now Is:             ${score}`));
+        }
+        else {
+            console.log(chalk.red("Invalid input!"));
+        }
+    }
+    ;
+}
+;
+guessingGame();
