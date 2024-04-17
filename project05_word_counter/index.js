2aa2c716de9e4e1e907fe5daaf16c891ca1a43b4
Syed-MuhammadRazaZaidi <160251042+Syed-MuhammadRazaZaidi@users.noreply.github.com>
2024-04-18 02:24:27 +0500

    Add files via upload
    The user will enter an english paragraph and all that is needed is to just to implement counting characters and words without whitespaces.

---
+++ project05_word_counter/index.js
@@ -0,0 +1,14 @@
+#! /usr/bin/env node
+// project03_todo_list:-
+import chalk from "chalk";
+import inquirer from "inquirer";
+console.log(chalk.cyan("Welcome To The Word Counter Game!"));
+let wordCounter = await inquirer.prompt([
+    {
+        name: "content",
+        type: "input",
+        message: "Please Write Something To Count Its Total Number Of Words: ",
+    }
+]);
+let contentArray = wordCounter.content.trim().split(" ");
+console.log(`The Words Count Of This Content is: \t ${chalk.yellow(contentArray.length)}`);
---
+++ project05_word_counter/index.ts
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+
+// project03_todo_list:-
+
+import chalk from "chalk";
+import inquirer from "inquirer";
+
+console.log(chalk.cyan("Welcome To The Word Counter Game!"));
+
+let wordCounter: {content: string} = await inquirer.prompt([
+    {
+        name: "content",
+        type: "input",
+        message: "Please Write Something To Count Its Total Number Of Words: ",
+    }
+])
+
+let contentArray = wordCounter.content.trim().split(" ");
+console.log(`The Words Count Of This Content is: \t ${chalk.yellow(contentArray.length)}`);
+
